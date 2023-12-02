e7d84e27452d8713f87b8a5b26d0bb0aadac6a80
Noman73 <noman.eng73@gmail.com>
2023-12-02 18:43:17 +0600

    02-12-2023

--- src/resources/views/easycrud/crud_maker.blade.php
+++ src/resources/views/easycrud/crud_maker.blade.php
@@ -60,7 +60,7 @@
           </div>
       </div><!-- /.container-fluid -->
       {{-- modal --}}
-      <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" id="modal">
+      <div class="modal fade bd-example-modal-lg" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" id="modal">
         <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header">
